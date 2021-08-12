339bcdc3d432653363d0a0648460ef6f10f54e22
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-08-12 04:24:15 -0400

    adding the javascript variables

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ let min = 1;
 let max = 10;
 let win = 2;
 let guessNum = 3;
+
+
+const game = document.querySelector('.game');
+const minNum =document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guesIn = document.querySelector('#guess');
+const message = document.querySelector('.result');
+
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click' , theGuess)
+
+function theGuess(){
+    let guess = parseInt(guesIn.value)
+    alert(guess)
+}
